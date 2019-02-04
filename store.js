d309ab13eebcf08950be2adf54fd72d8472d40bc
Bogdan Moroz <bogdan.moroz@observis.fi>
2019-02-04 12:17:20 +0200

    store and defaultState created

--- store.js
+++ store.js
@@ -1,1 +1,21 @@
 import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+//Import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//Create an objecct for the default data
+const defautState = {
+  posts,
+  comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
