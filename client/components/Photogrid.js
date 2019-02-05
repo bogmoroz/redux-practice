a951b32d4c4dc8fc0bf4534fd05b81ae987abf8a
Bogdan Moroz <bogdan.moroz@observis.fi>
2019-02-05 15:38:38 +0200

    Photo component

--- client/components/Photogrid.js
+++ client/components/Photogrid.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
-    return <div className="photo-grid">I'm photo grid</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, i) => (
+          <Photo {...this.props} key={i} i={i} post={post} />
+        ))}
+      </div>
+    );
   }
 });
 
